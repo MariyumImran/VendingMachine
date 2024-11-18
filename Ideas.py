9ba325d1a74f3b3040e9fb3123ab0c9f2f3b9ac1
mariyum imran <mariyumimran247@gmail.com>
2024-11-18 14:06:45 -0400

    Chips done

--- Ideas.py
+++ Ideas.py
@@ -1,5 +1,10 @@
 """
 Items:
-Chips: Pringles, Lays, Ruffles, Takis
-Pringles
+Chips: Pringles, Lays, Cheetos, Takis
+Pringles: Original, Ranch, Cheddar Cheese, Sour Cream & Onion
+Lays: Classic, Barbecue, Wavy, Sour Cream & Onion
+Cheetos: Puffs(Original), Flamin' Hot, Crunchy, Minis
+Talkis: Fuego(Original), Nitro, Intense Nachos, Blue Heat
+
+
 """
