ebbd2f519b25d4b5fa8785b020ec4e686fee89aa
mariyum imran <mariyumimran247@gmail.com>
2024-12-15 16:02:01 -0400

    Update #2 Ideas.py

--- Ideas.py
+++ Ideas.py
@@ -1,6 +1,8 @@
 """  
 Items:
 
+Chips, Chocolates, Drinks, 
+
 Chips: Pringles, Lays, Cheetos, Takis
 
 Pringles: Original, Ranch, Cheddar Cheese, Sour Cream & Onion
@@ -8,8 +10,12 @@ Lays: Classic, Barbecue, Wavy, Sour Cream & Onion
 Cheetos: Puffs(Original), Flamin' Hot, Crunchy, Minis
 Takis: Fuego(Original), Nitro, Intense Nachos, Blue Heat
 -----------------------------------------------------------------------------
-Chocolates: Mars, Snickers, Kit Kat, Hershey
+Chocolates: Mars, M&Ms, Kit Kat, Hershey
 
 Mars: Original, Caramel, Fudge, Caramel Sundae
 Hershey: Milk Chocolate (Original), Cookies n' Creme, Dark Chocolate, Caramel
+M&Ms: Milk Chocolate (Origianl), Almond, Caramel, Peanut
+Kit Kat: Original, White Chocolate, Caramel, Strawberry
+----------------------------------------------------------------------------- 
+Drinks: Soda, Juice, 
 """
